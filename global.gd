b7f288e618cd8c40506fd07ad93de964cbf1af46
Mrizzi-96 <rizzimarco96@hotmail.it>
2024-09-12 22:08:27 +0200

    feat(global): add random scene switcher when pressing space key

--- global.gd
+++ global.gd
@@ -1,12 +1,22 @@
 extends Node
 
 var current_scene = null
-@onready var quit_button = $VBoxContainer/VBoxContainer/Quit
+var waves_left: int
+var level_path: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() -1)
+	level_path = "res://Levels/arena"
+
+func _process(delta):
+	# TODO: after wave completed, goto random scene
+	if Input.is_key_pressed(KEY_SPACE):
+		load_random_arena_scene()
+		# TODO: bind not here, but in pause menu on button pressed
+	if Input.is_key_pressed(KEY_Q):
+		goto_scene("res://MainMenu/main_menu.tscn")
 
 func goto_scene(path):
 	call_deferred("_deferred_goto_scene", path)
@@ -17,6 +27,11 @@ func _deferred_goto_scene(path):
 	current_scene = s.instantiate()
 	get_tree().root.add_child(current_scene)
 	get_tree().current_scene = current_scene
+
+func load_random_arena_scene():
+	var rand = RandomNumberGenerator.new().randi_range(1,3)
+	goto_scene(level_path + str(rand) + ".tscn")
+
 
 func quit_game():
 	get_tree().quit()
