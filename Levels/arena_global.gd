74e943dc09d5416de6ca8370ad095dfff6bcd8ca
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2024-09-17 22:53:23 +0200

    feat(arena): connect game_ended event to basic method and add player_spawner in all scenes

--- Levels/arena_global.gd
+++ Levels/arena_global.gd
@@ -2,10 +2,14 @@ extends Node2D
 
 var one_time_only = false
 var wave_enemies : Array
+var player_obj
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	wave_enemies = [load("res://Enemies/enemy_base.tscn")]
-	# TODO: attach this code to every single Arena
+	$PlayerSpawner.spawn_player() 
+	player_obj = $Player
+	player_obj.connect("game_ended", _on_game_ended)
 	# TODO: instantiate the spawners (and their enemies) accordingly
 	pass
 
@@ -17,3 +21,9 @@ func _process(delta):
 		one_time_only = true
 		$enemySpawner.enemyToSpawn = wave_enemies[0]
 		$enemySpawner._spawnEnemy(1, 1)
+
+func _on_game_ended():
+	_show_game_over_UI()
+
+func _show_game_over_UI():
+	print("Signal emitted! Simon says: GAME OVER!")
