d03af20a8d33aa3603dae2f326edb10903facad3
IlJeko <60620724+IlJeko@users.noreply.github.com>
2024-12-11 03:10:56 +0100

    feat(slashing_weapon): added logic and renamed knife into slashing_weapon_base + fixed directory

---
+++ Weapons/SlashingWeaponBase/slashing_weapon_base.gd
@@ -0,0 +1,17 @@
+extends RigidBody2D
+
+class_name SlashingWeapon
+
+@export var slash_strength = 5000
+
+var knife
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _unhandled_input(_event) -> void:
+	if Input.is_action_just_pressed("act") && Global.active_leg.name == "LeftLeg":
+		act()
+
+func act() -> void:
+	add_impulse_player()
+
+func add_impulse_player():
+	$"../../..".apply_impulse(Vector2(slash_strength, 0).rotated($"..".global_rotation))
