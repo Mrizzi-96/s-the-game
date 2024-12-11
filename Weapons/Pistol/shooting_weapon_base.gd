ae204cdb29cc7fe1afbb997dd0d9b6b429686d62
IlJeko <60620724+IlJeko@users.noreply.github.com>
2024-12-11 02:56:30 +0100

    chore(shooting_weapon): refactor

--- Weapons/Pistol/shooting_weapon_base.gd
+++ Weapons/Pistol/shooting_weapon_base.gd
@@ -6,20 +6,20 @@ class_name ShootingWeapon
 @export var recoil_force = 2000
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _unhandled_input(_event):
+func _unhandled_input(_event) -> void:
 	if Input.is_action_just_pressed("act") && Global.active_leg.name == "RightLeg":
 		act()
 
-func act():
+func act() -> void:
 	spawn_bullet()
 	add_impulse_player()
 
-func spawn_bullet():
+func spawn_bullet() -> void:
 	var bullet = player_bullet.instantiate()
 	bullet.position =$BulletSpawn.global_position
 	bullet.rotation =$BulletSpawn.global_rotation 
 	get_tree().root.add_child(bullet)
 	
-func add_impulse_player():
+func add_impulse_player() -> void:
 	$"../../..".apply_impulse(Vector2(-recoil_force, 0).rotated($"..".global_rotation))
-	#  ^Hips node										 ^RightLeg node
+	#  ^Hips node										 		  ^RightLeg node
