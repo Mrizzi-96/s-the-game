e298d46b9ee32a53c8fd5dabdad6a79659369c49
IlJeko <60620724+IlJeko@users.noreply.github.com>
2024-12-16 20:27:52 +0100

    chore(Global): Added automatic weapons dictionary

--- global.gd
+++ global.gd
@@ -5,12 +5,19 @@ var wave: int
 var level_path: String
 var enemyNum: int
 var active_leg
+var right_weapon
+var left_weapon
+var weapons = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var root = get_tree().root
 	current_scene = root.get_child(root.get_child_count() -1)
 	level_path = "res://Levels/arena"
+	
+	var weapon_dir = "res://Weapons/"
+	scan_directory(weapon_dir)
+	print(weapons)  # Debug: Check the generated dictionary
 
 func _process(delta):
 	# TODO: after wave completed, goto random scene
@@ -37,3 +44,24 @@ func load_random_arena_scene():
 
 func quit_game():
 	get_tree().quit()
+
+#Weapon dictonary automatic generation
+func scan_directory(path: String):
+	var dir = DirAccess.open(path)
+	if dir:
+		dir.list_dir_begin()
+		var file_name = dir.get_next()
+		while file_name != "":
+			if file_name != "." and file_name != "..":
+				var entry_path = path + file_name
+				if dir.current_is_dir():
+					if file_name != "Assets" and file_name != "Bullet":
+						scan_directory(entry_path + "/")
+				else:
+					if file_name.ends_with(".tscn"): 
+						var weapon_name = file_name.get_basename()
+						weapons[weapon_name] = entry_path
+			file_name = dir.get_next()  
+		dir.list_dir_end()  # Close the directory listing
+	else:
+		print("An error occurred when trying to access the path: " + path)
