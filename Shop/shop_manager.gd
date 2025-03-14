357f2267c18a9a8aac8498363f1303bae9a4576b
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-14 18:37:45 +0100

    refactor(shop): use RunInfo to load resources

--- Shop/shop_manager.gd
+++ Shop/shop_manager.gd
@@ -6,18 +6,10 @@ var card_base_scene = preload("res://Shop/item_card.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	# load item resources as array of resources
-	var items_path = []
-	for i in DirAccess.get_files_at("res://Resources/Items"):
-		if 	i.ends_with(".gd"):
-			continue
-		var item_full_path = item_resources_folder_path + i 
-		items_path.append(item_full_path)
 		
-	for i in items_path.size():
-		var item = load(items_path[i])
+	for i in RunInfo.items.size():
 		var instCardScn = card_base_scene.instantiate() as ItemShopCard
-		instCardScn.init(item, Vector2(128,128))
+		instCardScn.init(RunInfo.items[i], Vector2(128,128))
 		%ShopGrid.add_child(instCardScn)
 
 
