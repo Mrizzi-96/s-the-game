25a383235920042350a3a9d36ed2402f375a722c
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-14 18:36:24 +0100

    chore(): add Autoloads folder

---
+++ Autoloads/run_info.gd
@@ -0,0 +1,20 @@
+extends Node
+
+### This class contains all properties from the run, like the number of rounds played, the available items etc 
+const ITEMDATA_DIR_PATH : String = "res://Resources/Items/"
+
+@export var items : Array[ItemData]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	populate_items()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func populate_items() -> void:
+	for i in DirAccess.get_files_at(ITEMDATA_DIR_PATH):
+		var item = ResourceLoader.load(ITEMDATA_DIR_PATH + i) as ItemData
+		items.append(item)
