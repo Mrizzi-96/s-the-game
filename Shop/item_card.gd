8a2bdaefc8c1ecdea4e3f0b3e26fd7f1952c1e53
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-16 15:11:29 +0100

    feat(shop): disable buy buttons if price is higfher than available money

--- Shop/item_card.gd
+++ Shop/item_card.gd
@@ -5,6 +5,7 @@ var item_data : ItemData
 
 @onready var image_texture = %ImageTexture
 @onready var item_price = %ItemPrice
+@onready var buy_button : Button = %BuyButton
 
 func init(item: ItemData, cms : Vector2):
 	if not is_node_ready():
@@ -17,7 +18,20 @@ func init(item: ItemData, cms : Vector2):
 	
 	# set price
 	item_price.set_text(str(item_data.price))
+	# disable buy button if player has not enough money
+	buy_button.disabled = item_data.price > RunInfo.player_gold
+		
 
 func buy():
-	if item_data.price > 0:
+	if item_data.price > RunInfo.player_gold:
 		print("NOT ABLE TO BUY")
+	else:
+		# remove item price from player gold
+		RunInfo.player_gold = RunInfo.player_gold - item_data.price
+		if	RunInfo.player_gold < 0:
+			RunInfo.player_gold = 0
+			
+		# TODO: add to RunInfo player_inventory
+		 
+		# remove card from shop
+		queue_free()
