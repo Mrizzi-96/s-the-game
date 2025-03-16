096d8e9b39fcb7e95b4240d349574392000630f7
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-16 15:12:06 +0100

    feat(shop): init player gold in RunInfo autoload

--- Autoloads/run_info.gd
+++ Autoloads/run_info.gd
@@ -4,10 +4,14 @@ extends Node
 const ITEMDATA_DIR_PATH : String = "res://Resources/Items/"
 
 @export var items : Array[ItemData]
+@export var player_gold : int
+@export var player_inventory: Array[ItemData]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	populate_items()
+	# on first init, set player gold to const value
+	player_gold = 1000
 
 func populate_items() -> void:
 	for i in DirAccess.get_files_at(ITEMDATA_DIR_PATH):
