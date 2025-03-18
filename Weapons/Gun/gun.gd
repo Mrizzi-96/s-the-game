dff3f1cc128e094f55dad374da881e15c880e69c
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-19 00:38:34 +0100

    fix(weapons): use base methods in basic weapons

--- Weapons/Gun/gun.gd
+++ Weapons/Gun/gun.gd
@@ -4,7 +4,4 @@ func _ready():
 	item_data = ResourceLoader.load("res://Resources/Items/gun.tres")
 
 func spawn_bullet() -> void:
-	var bullet = player_bullet.instantiate()
-	bullet.position =$BulletSpawn.global_position
-	bullet.rotation =$BulletSpawn.global_rotation 
-	get_tree().root.add_child(bullet)
+	super.spawn_bullet()
--- Weapons/Knife/knife.gd
+++ Weapons/Knife/knife.gd
@@ -4,4 +4,4 @@ func _ready():
 	item_data = ResourceLoader.load("res://Resources/Items/knife.tres")
 	
 func add_impulse_player():
-	$"../../../..".apply_impulse(Vector2(slash_strength, 0).rotated($"../..".global_rotation))
+	super.add_impulse_player()
