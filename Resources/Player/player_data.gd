93645621c5a8fcaa1fd707f75b9548cb45f2a87c
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-18 23:46:31 +0100

    feat(player_data): add add_to_inventory function

--- Resources/Player/player_data.gd
+++ Resources/Player/player_data.gd
@@ -5,6 +5,9 @@ class_name PlayerData extends Resource
 @export var equipment : Dictionary # here, we set a dictionary with "left_leg" / "right_leg" and ItemData as value.
 @export var health : int
 
-
 func add_to_equipment(leg: String, item_data : ItemData):
 	equipment[leg] = item_data
+	
+func add_to_inventory(item_data:ItemData):
+	if not inventory.has(item_data):
+		inventory.append(item_data)
