860b1d7964036a7f92465e3ae003f9d78c3f4a79
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-18 23:10:15 +0100

    feat(player): add PlayerData resource and initialize with a starting resource

---
+++ Resources/Player/player_data.gd
@@ -0,0 +1,10 @@
+class_name PlayerData extends Resource
+
+@export var gold : int
+@export var inventory: Array[ItemData]
+@export var equipment : Dictionary # here, we set a dictionary with "left_leg" / "right_leg" and ItemData as value.
+@export var health : int
+
+
+func add_to_equipment(leg: String, item_data : ItemData):
+	equipment[leg] = item_data
