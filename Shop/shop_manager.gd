1740d2609cd8072cfbbbe123f8fed13bc4fde6e9
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-19 00:21:14 +0100

    feat(ui): add inventory setup to shop

--- Shop/shop_manager.gd
+++ Shop/shop_manager.gd
@@ -1,16 +1,24 @@
 extends Control
 
-@export var ITEMS_PER_ROW = 12
 var card_base_scene = preload("res://Shop/item_card.tscn")
+var inv_card_scene = preload("res://Shop/inventory_card.tscn")
+
 @onready var item_resources_folder_path = "res://Resources/Items/"
+@onready var player_data = RunInfo.player_data
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-		
+	# populate shop
 	for i in RunInfo.items.size():
 		var instCardScn = card_base_scene.instantiate() as ItemShopCard
 		instCardScn.init(RunInfo.items[i], Vector2(128,128))
 		%ShopGrid.add_child(instCardScn)
+		
+	# do the same for player inventory
+	for i in player_data.inventory.size():
+		var invCard = inv_card_scene.instantiate() as InventoryCard
+		invCard.init(player_data.inventory[i], Vector2(128,128))
+		%InventoryGrid.add_child(invCard)
 
 
 
