b96b6cc80d0d07a7d924e6c31eb780be8553b82c
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-19 22:08:00 +0100

    fix(ui): use copy of player_data resource instead of prefab when pressing start game

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -5,10 +5,13 @@ extends MarginContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play.pressed.connect(Global.goto_scene.bind("res://ArenaChoice/arena_choice.tscn"))
 	quit.pressed.connect(Global.quit_game)
 
-
+func _start_game():
+	# use a copy of player_start as ther player data, to avoid using the "prefab". each time we start again, we'll have a different player data resource
+	RunInfo.player_data = ResourceLoader.load("res://Resources/Player/player_start.tres").duplicate()
+	Global.goto_scene("res://ArenaChoice/arena_choice.tscn")
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
