c1475a830dbb9d3cc08a9e2624ce759098399efb
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-03-29 14:32:00 +0100

    feat(): update tiles and add shootgun cooldown

--- Weapons/Shotgun/shotgun.gd
+++ Weapons/Shotgun/shotgun.gd
@@ -2,11 +2,20 @@ extends ShootingWeapon
 
 @export var shooting_angle = 30
 @export var bullet_number = 8
+@onready var shooting_cooldown = %ShootingCooldown
+var can_shoot: bool = true
 
 func spawn_bullet() -> void:
-	for i in range(bullet_number): # Spawn bullets
-		var bullet = player_bullet.instantiate()
-		bullet.position =$BulletSpawn.global_position
-		# Apply random rotation within the spread range
-		bullet.rotation = $BulletSpawn.global_rotation + randf_range(-deg_to_rad(shooting_angle), deg_to_rad(shooting_angle))
-		get_tree().root.add_child(bullet)
+	if can_shoot:
+		can_shoot = false
+		shooting_cooldown.start()
+		for i in range(bullet_number): # Spawn bullets
+			var bullet = player_bullet.instantiate()
+			bullet.position =$BulletSpawn.global_position
+			# Apply random rotation within the spread range
+			bullet.rotation = $BulletSpawn.global_rotation + randf_range(-deg_to_rad(shooting_angle), deg_to_rad(shooting_angle))
+			get_tree().root.add_child(bullet)
+
+
+func _on_shooting_cooldown_timeout():
+	can_shoot= true
