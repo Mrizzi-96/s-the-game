f6153cceee200c24e103d902374eba105328df3c
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2025-10-25 14:39:30 +0200

    fix(arena): use ArenaParams when clicking arena choice

--- ArenaChoice/arena_choice.gd
+++ ArenaChoice/arena_choice.gd
@@ -1,13 +1,37 @@
 extends Control
 
+@onready var arena_params = ArenaParams.new()
+# WARNING:
+# For now, on button click the arena params will be added as hardcoded.
+# @jadedpear will then use the ones from his arena_preview scene
+# see #TG-110 on Taiga 
+
 func back_to_menu():
 	Global.goto_scene("res://MainMenu/main_menu.tscn")
 
 func goto_arena_1():
-	Global.goto_scene("res://Levels/level1.tscn")
+	var arena_path = "res://Levels/level1.tscn"
+	arena_params.arena_scene = preload("res://Levels/level1.tscn")
+	arena_params.difficulty = 1
+	arena_params.reward_type = ArenaParams.RewardType.WEAPON
+	RunInfo.current_arena_params = arena_params
+	
+	Global.goto_scene(arena_path)
 
 func goto_arena_2():
-	Global.goto_scene("res://Levels/level2.tscn")
+	var arena_path = "res://Levels/level2.tscn"
+	arena_params.arena_scene = preload("res://Levels/level2.tscn")
+	arena_params.difficulty = 2
+	arena_params.reward_type = ArenaParams.RewardType.GOLD	
+	RunInfo.current_arena_params = arena_params
+	
+	Global.goto_scene(arena_path)
 
 func goto_arena_3():
-	Global.goto_scene("res://Levels/level3.tscn")
+	var arena_path = "res://Levels/level3.tscn"
+	arena_params.arena_scene = preload("res://Levels/level3.tscn")
+	arena_params.difficulty = 3
+	arena_params.reward_type = ArenaParams.RewardType.SKILL
+	RunInfo.current_arena_params = arena_params
+	
+	Global.goto_scene(arena_path)
