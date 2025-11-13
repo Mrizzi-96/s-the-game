3353da42aa5baf8ad33cdcadc505ec2b6ab6ffd2
Marco Carega <marcocarega.dev@gmail.com>
2025-11-13 19:40:58 +0100

    fix(RewardMenu): reward menu go to shop now

--- RewardMenu/reward_menu.gd
+++ RewardMenu/reward_menu.gd
@@ -3,5 +3,12 @@ extends PanelContainer
 
 @onready var _continue_button = $MarginContainer/VBoxContainer/BoxContainer/ContinueButton
 
+@onready var score_label=$MarginContainer/VBoxContainer/ScoreLabel
+
 func _on_continue_button_pressed() -> void:
-	Global.goto_scene("res://Shop/shop.tscn")
+	Global.goto_scene("res://ArenaChoice/arena_choice.tscn")
+
+func _ready() -> void:
+	var score=RunInfo.total_score
+	score_label.text="Score: "+str(score)
+	
