b27a6a95cc948a2cf84a15dc8feaee8509324311
IlJeko <60620724+IlJeko@users.noreply.github.com>
2025-11-13 17:52:18 +0100

    feat(enemy base): Add blood splash when hit

--- Enemies/BaseEnemy/enemy_base.gd
+++ Enemies/BaseEnemy/enemy_base.gd
@@ -5,6 +5,8 @@ extends Node2D
 # the script and create a new one using extends class_name
 class_name Enemy
 
+var hit_particles = preload("res://Enemies/BaseEnemy/hit_particle.tscn")
+
 var hp: int = 100
 var movement_speed: float = 1.0
 
@@ -26,9 +28,15 @@ func _setSpeed(speed: float) -> void:
 func _hit(damage: int) -> void:
 	$HitFlashAnimationPlayer.play("hit_flash")
 	DamageNumbers.display_damage(damage, $RigidBody2D/ParticlePosition.global_position)
+	var splat = hit_particles.instantiate()
+	splat.global_position = $RigidBody2D.global_position
+	splat.emitting = true
+	get_tree().current_scene.add_child(splat)
 	hp -= damage
 	if hp <= 0:
 		_death()
+	await get_tree().create_timer(splat.lifetime).timeout
+	splat.queue_free()
 	
 	
 func _death() -> void:
