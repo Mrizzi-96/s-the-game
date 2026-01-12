87d2a23d29d6316740d9960928391f1876b888c3
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2026-01-13 00:32:42 +0100

    feat(ui): add basic pause menu

--- Levels/pause_component.gd
+++ Levels/pause_component.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 @onready var _is_paused: bool = false
 
 func _ready() -> void:
-	process_mode = Node.PROCESS_MODE_PAUSABLE
+	get_tree().root.process_mode = Node.PROCESS_MODE_PAUSABLE
 	# hide canvas layer
 	self.visible = false
 
@@ -13,3 +13,14 @@ func _process(_delta: float) -> void:
 		get_tree().paused = _is_paused
 		# show/hide canvas layer
 		self.visible = _is_paused
+
+
+func _on_resume_button_up() -> void:
+	_is_paused = false
+	self.visible = _is_paused
+	get_tree().paused = _is_paused
+
+
+func _on_main_menu_button_up() -> void:
+	get_tree().paused = false
+	Global.goto_scene("uid://hp147ctfpi5o")
