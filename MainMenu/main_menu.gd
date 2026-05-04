12cbf6d17920cb66bffb8634fdef51c43c7bf3a7
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2026-05-04 22:04:23 +0200

    feat(ui): add new main menu UI and wip scene for testing

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -1,22 +1,25 @@
 extends Control
 
-@onready var play = %Play
-@onready var quit = %Quit
+const WIP_SCENE : String = "uid://xgwpv7t6h6ok"
+const ARENA_CHOICE_SCENE: String = "uid://c4yvx7bjrrwf0"
 
+@onready var play : Button = %Play
+@onready var quit : Button = %Quit
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	quit.pressed.connect(quit_game)
 	# reset arena count, player and run info
 	RunInfo.reset()
 
 func _start_game():
 	# use a copy of player_start as ther player data, to avoid using the "prefab". each time we start again, we'll have a different player data resource
 	RunInfo.player_data = ResourceLoader.load("res://Resources/Player/player_start.tres").duplicate()
-	Global.goto_scene("res://ArenaChoice/arena_choice.tscn")
-	
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta):
-	pass
+	Global.goto_scene(ARENA_CHOICE_SCENE)
 
-func quit_game():
+func _on_quit_button_up() -> void:
 	Global.quit_game()
+
+func _on_how_to_play_button_up() -> void:
+	Global.goto_scene(WIP_SCENE)
+
+func _on_settings_button_up() -> void:
+	Global.goto_scene(WIP_SCENE)
---
+++ wip_scene.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_main_menu_button_up() -> void:
+	Global.goto_scene("res://MainMenu/main_menu.tscn")
