612fe60288742ad4e6e8106162d7536593fab8c8
Marco Carega <marcocarega.dev@gmail.com>
2026-05-16 17:24:34 +0200

    feat(test): add test arena

--- MainMenu/main_menu.gd
+++ MainMenu/main_menu.gd
@@ -8,6 +8,12 @@ const ARENA_CHOICE_SCENE: String = "uid://c4yvx7bjrrwf0"
 @onready var quit : Button = %Quit
 # Called when the node enters the scene tree for the first time.
 func _ready():
+@export var DEBUG_MODE:bool=false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if(not DEBUG_MODE):
+		%Test.queue_free()
 	# reset arena count, player and run info
 	RunInfo.reset()
 
@@ -30,3 +36,10 @@ func _on_yes_button_button_up() -> void:
 
 func _on_no_button_button_up() -> void:
 	confirmation_overlay.visible = false
+
+
+func _on_test_pressed() -> void:
+	# Load the player data from the resource file, duplicate it to avoid modifying the original resource, and assign it to RunInfo.player_data. Then, create a new instance of ArenaParams and assign it to RunInfo.current_arena_params. Finally, navigate to the arena test scene.
+	RunInfo.player_data = ResourceLoader.load("res://Resources/Player/player_start.tres").duplicate()
+	RunInfo.current_arena_params=ArenaParams.new()
+	Global.goto_scene("res://Levels/arena_test.tscn")
