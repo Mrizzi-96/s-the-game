ddcdbc4a8d6b197b0906cf20b0e7708c72874f67
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2026-05-28 19:52:23 +0200

    feat(mosquito): add mosquito animations and sprites

--- Enemies/FlyingFry/flying_fry.gd
+++ Enemies/FlyingFry/flying_fry.gd
@@ -1,3 +1,8 @@
 extends Enemy
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var mosquito_sprite: AnimatedSprite2D = %mosquito_sprite
+
+func _death():
+	super._death()
+	mosquito_sprite.stop()
+	mosquito_sprite.play("death")
