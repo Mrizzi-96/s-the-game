45abe706c9c58446f011508d4a84f84dc61aba3f
Marco Camillo Rizzi <58190165+Mrizzi-96@users.noreply.github.com>
2026-08-29 15:40:42 +0200

    fix(levels): remove cursed X levels

--- Autoloads/run_info.gd
+++ Autoloads/run_info.gd
@@ -9,7 +9,7 @@ const ITEMDATA_DIR_PATH : String = "res://Resources/Items/"
 
 @export var arena_counter: int = 0
 ## The number of the currently playable arenas
-@export var arena_playable: int = 6
+@export var arena_playable: int = 5
 # this will be populated on choosing the correct arena
 @export var current_arena_params: ArenaParams
 
